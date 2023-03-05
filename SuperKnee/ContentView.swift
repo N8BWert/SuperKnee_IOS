86af6858aba63ea6fee39860f322e470ed7cdb23
Miles Gordon <gordonmiles03@gmail.com>
2023-03-04 23:06:43 -0500

    Commit

--- SuperKnee/ContentView.swift
+++ SuperKnee/ContentView.swift
@@ -1,21 +1,62 @@
 //
 //  ContentView.swift
-//  SuperKnee
+//  Consumer Exoskeleton
 //
-//  Created by nate wert on 3/4/23.
+//  Created by mgordon on 3/4/23.
 //
 
 import SwiftUI
+import Charts
+import Foundation
+import CoreBluetooth
+import OSLog
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+        }
+        .padding()
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BarChart()
     }
 }
+
+struct BarChart: View {
+    var body: some View {
+        VStack {
+            GroupBox ( "This is a graph") {
+                Chart {
+                    ForEach(myGraph) {
+                        LineMark(
+                            x: .value("Time", $0.x),
+                            y: .value("Force", $0.y)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct XYPair: Identifiable {
+    let id = UUID()
+    let x: Double
+    let y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
+
+
+let myGraph: [XYPair] = [
+    XYPair(x: 1, y: 1),
+]
+
