2dff5341925763f9386eb2da20afddfb92109522
N8BWert <n8.wert.b@gmail.com>
2023-03-04 22:57:17 -0500

    Initial Commit

---
+++ SuperKnee/SuperKneeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKneeApp.swift
+//  SuperKnee
+//
+//  Created by nate wert on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperKneeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
